c15428a0663b45e9869df1c6f1dabad8a7ddf321
BraIN <brainsvoboda@mail.ru>
2023-01-25 17:25:39 +0300

    Task 1 complete

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+System.Console.Write("Введите число A: ");
+int a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число B: ");
+int b=Convert.ToInt32(Console.ReadLine());
+int summ=a;
+for (int i=1; i<b; i++){
+    summ=summ*a;
+}
+System.Console.WriteLine($"Число {a} в степени {b} = {summ}");
