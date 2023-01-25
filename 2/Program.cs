700310a5a1da4e4e893d95f9f8bf0ba9c46b393b
BraIN <brainsvoboda@mail.ru>
2023-01-25 17:32:35 +0300

    Task 2 complete

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+System.Console.Write("Введите число: ");
+string chislo=Console.ReadLine();
+int summ=0;
+
+for (int i=0; i<chislo.Length;i++){ 
+    summ=Convert.ToInt32(chislo[i].ToString())+summ;
+}
+System.Console.WriteLine($"Сумма всех цифр числа {chislo} = {summ}");
